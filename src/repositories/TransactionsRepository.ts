3b8126dd353dbdeab4f753b8f26a79b49eb47152
Eduardo Lainetti <eduardolainetti2@hotmail.com>
2020-04-16 21:57:23 -0300

    Initial commit

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,31 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const incomeSum = this.transactions
+      .filter(transaction => transaction.type === 'income')
+      .reduce((sum, current) => sum + current.value, 0);
+
+    const outcomeSum = this.transactions
+      .filter(transaction => transaction.type === 'outcome')
+      .reduce((sum, current) => sum + current.value, 0);
+
+    return {
+      income: incomeSum,
+      outcome: outcomeSum,
+      total: incomeSum - outcomeSum,
+    };
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
